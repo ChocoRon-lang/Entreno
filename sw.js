73812119703ea0e19a38ec9b69f145f70db1c9fe
ChocoRon-lang <hansurzuafonseca@gmail.com>
2026-08-17 15:18:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,3 @@
-// Minimal service worker for Entreno.
-// Enables system-style notification banners (via showNotification).
-
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(
-    self.clients.matchAll({ type: 'window' }).then((clientList) => {
-      for (const client of clientList) {
-        if ('focus' in client) return client.focus();
-      }
-      if (self.clients.openWindow) return self.clients.openWindow('./');
-    })
-  );
-});
+{
+  "version": "2026-08-13.2"
+}
